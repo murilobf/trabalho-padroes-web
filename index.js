650fb8b93947850ebf95330df80cafe8b783b035
Murilo <murilobontorin2005@gmail.com>
2025-10-29 17:34:19 -0300

    Início da funcionalidade da sidebar

---
+++ index.js
@@ -0,0 +1,9 @@
+const barraLeteral = document.querySelector(".sidebar")
+
+function abrirBarraLateral(){
+    barraLeteral.style.width = "10%";
+}
+
+function fecharBarraLateral(){
+    barraLeteral.style.width = "0";
+}
