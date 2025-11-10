62439b881ad3e28ebbc72e428467238cb1067064
Murilo <murilobontorin2005@gmail.com>
2025-11-10 12:23:17 -0300

    Inicio página de relatório (falta js)

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const barraLateral = document.querySelector(".sidebar")
+const quiz = document.querySelector("form")
 
 function menuClicado(){
     barraLateral.classList.toggle('open');
 }
 
+function pegaResposta(){
+    const resposta = document.querySelector('input[name="quiz1"]:checked').value;
+
+    console.log(resposta)
+}
