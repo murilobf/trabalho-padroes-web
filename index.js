a3d4bbca8db42382fdd85d13e0d73f9d8904f821
Murilo <murilobontorin2005@gmail.com>
2025-11-12 09:42:14 -0300

    Adicionado alt nas imagens

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const barraLateral = document.querySelector(".sidebar")
-const quiz = document.querySelector("form")
 
 function menuClicado(){
     barraLateral.classList.toggle('open');
 }
 
+const quiz = document.querySelector("form")
 function pegaResposta(num){
     const resposta = document.querySelector(`input[name="quiz${num}"]:checked`).value;
 
-    //TODO: Pegar qual seria a resposta certa (talvez guardar também as que não estejam tão erradas), verificar a resposta marcada e bater com a certa
+    //TODO: Pegar a resposta certa, verificar a resposta marcada e se elas batem
+    //Então, aumentar a quantidade de acertos/meio acertos (se aplicável)
 }
+
+//TODO: Fazer a função que modifica a página de relatório com base nas respostas
